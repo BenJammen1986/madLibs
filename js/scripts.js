de2706d9cf7ae408bcc4849898ba84b421a701cb
Ben Schenkenberger and Josh Linton <ben@>
2017-02-20 11:58:36 -0800

    refactor to DRY code: Add array & .forEach loop to eliminate unnecessarily complicated code

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
+var totalInput = [];
+
 $(document).ready(function() {
-  $("#formOne").submit(function() {
+    $("#formOne").submit(function() {
+        event.preventDefault();
+        totalInput = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-    $("#story").show();
+        totalInput.forEach(function(inputItem) {
+            var userInput = $("input#" + inputItem).val();
+            $("." + inputItem).text(userInput);
+        })
 
-    event.preventDefault();
+        $("#story").show();
 
-  });
+        // This array eliminated all of this code! :-)
+        // var person1Input = $("input#person1").val();
+        // var person2Input = $("input#person2").val();
+        // var animalInput = $("input#animal").val();
+        // var exclamationInput = $("input#exclamation").val();
+        // var verbInput = $("input#verb").val();
+        // var nounInput = $("input#noun").val();
+        //
+        // $(".person1").text(person1Input);
+        // $(".person2").text(person2Input);
+        // $(".animal").text(animalInput);
+        // $(".exclamation").text(exclamationInput);
+        // $(".verb").text(verbInput);
+        // $(".noun").text(nounInput);
+
+    });
 });
